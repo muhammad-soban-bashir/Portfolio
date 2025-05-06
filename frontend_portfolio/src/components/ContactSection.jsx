c308851bd06a73768d1c5e2851bdc8c41c92611b
muhammad-soban-bashir <sobankhan2895@gmail.com>
2025-05-06 15:57:22 +0500

    url updated

--- frontend_portfolio/src/components/ContactSection.jsx
+++ frontend_portfolio/src/components/ContactSection.jsx
@@ -27,7 +27,7 @@ export const ContactSection = () => {
     };
   
     try {
-      const response = await fetch("https://portfolio-abc6.onrender.com", {
+      const response = await fetch("https://portfolio-abc6.onrender.com/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
