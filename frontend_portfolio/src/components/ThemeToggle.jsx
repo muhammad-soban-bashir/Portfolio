e5df14f621ac085659b84b36a96cca8985289324
muhammad-soban-bashir <sobankhan2895@gmail.com>
2025-05-06 16:47:26 +0500

    theme updated

--- frontend_portfolio/src/components/ThemeToggle.jsx
+++ frontend_portfolio/src/components/ThemeToggle.jsx
@@ -6,11 +6,20 @@ export const ThemeToggle = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
+    const isMobile = window.innerWidth <= 640; // Tailwind's "sm" breakpoint
+
     const storedTheme = localStorage.getItem("theme");
-    if (storedTheme === "dark") {
-      setIsDarkMode(true);
+
+    if (isMobile) {
+      // Always dark on small screens
       document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
+    } else if (storedTheme === "dark") {
+      document.documentElement.classList.add("dark");
+      setIsDarkMode(true);
     } else {
+      document.documentElement.classList.remove("dark");
       localStorage.setItem("theme", "light");
       setIsDarkMode(false);
     }
@@ -32,8 +41,7 @@ export const ThemeToggle = () => {
     <button
       onClick={toggleTheme}
       className={cn(
-        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-        "focus:outlin-hidden"
+        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300"
       )}
     >
       {isDarkMode ? (
